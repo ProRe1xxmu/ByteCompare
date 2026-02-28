d9889dec41a94d9c81621eb055b3c343a8800993
ProRe1xxmu <re1xxmu@icloud.com>
2026-02-28 18:17:42 +0300

    script now supports flags! use -region to set comparble region

--- bcomp.py
+++ bcomp.py
@@ -1,28 +1,52 @@
 import sys
+from os import path
 import hashlib
 import binascii
 from colorama import Back
 
-lfile=0
-rfile = 0
+lfile = ""
+rfile = ""
+region = [0, -1]
+argCounter = 0
 
-try:
-	with open(sys.argv[1], 'rb') as file:
-		lfile = bytearray(file.read())
-	with open(sys.argv[2], 'rb') as file:
-		rfile = bytearray(file.read())
-except Exception as e:
-	exit(f"can't open file/s: {e}")
+
+def reading(FilePath):
+	try:
+		with open(FilePath, "rb") as File:
+			File.read(region[0] * 16)
+			if region[1] < 0:
+				return bytearray(File.read(path.getsize(FilePath)))
+			return bytearray(File.read((region[1] - region[0] + 1) * 16))
+	except Exception as e:
+		exit(f"{Back.RED}Error{Back.BLACK}:{e}")
+
+for arg in sys.argv:
+	if arg == "-h" or "-help":
+		exit(f"ByteCompare - Compare files, see difference\n" +
+		"Usage: python3 bcomp.py -region 0xFROM-0xTO -lf path/to/first_file -rf path/to/second_file\n" + 
+		"\nFLAGS:\n-region(OPTIONAL): Script uses 16byte lines, -region flag sets region(in lines), which"+
+		" script will compare in files. if -region is not presented, script compares both files completly"+
+		"\n-lf: first file to compare\n-rf: second file to compare")
+	if arg == "-lf":
+		lfile = reading(sys.argv[argCounter + 1])
+	if arg == "-rf":
+		rfile = reading(sys.argv[argCounter + 1])
+	if arg == "-region":
+		map = sys.argv[argCounter + 1].split("-")
+		region[0] = int(map[0], 16)
+		region[1] = int(map[1], 16)
+		if region[1] - region[0] < 0:
+			exit(f"{Back.RED}WRONG REGION{Back.BLACK}: set region FROM-TO.")
+	argCounter = argCounter + 1
+
 llen = len(lfile)
 if llen != len(rfile):
 	exit("files should be same size!")
 lhsh = hashlib.md5(lfile).hexdigest()
 if lhsh == hashlib.md5(rfile).hexdigest():
-	exit(f"files hash is the same: {lhsh}")
+	exit(f"files hash is the same, files are {Back.GREEN}EQUAL TO EACH OTHER{Back.BLACK}: {lhsh}")
 
 i=0
-diff = []
-print(f"line\t\t {sys.argv[1].split("/")[-1]} \t\t\t\t\t\t {sys.argv[2].split("/")[-1]}")
 try:
 	while i < llen / 16:
 		ls = i*16
@@ -41,7 +65,7 @@ try:
 					l_form.append(format(ll[j], '04x')[2:].upper() + ' ')
 					r_form.append(format(rl[j], '04x')[2:].upper() + ' ')
 				j = j+1
-			print(f"{i:08x} \t {''.join(l_form)} \t {''.join(r_form)}")
+			print(f"{(i + region[0])*16:08x} \t {''.join(l_form)} \t {''.join(r_form)}")
 		i = i+1
 
 except Exception as e:
